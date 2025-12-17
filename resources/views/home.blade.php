35b8eb216ac6ea09c385be1c9713d613ddb36c5e
meldawonu <trimeldawanim@gmail.com>
2025-12-17 11:37:55 +0700

    fix: style auth and layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,3 @@
 <x-layout>
     <x-slot:title>PasundanPadel by NEEDSCRYPT</x-slot:title>
-<<<<<<< HEAD
-
-    {{-- @include('sections.hero')
-    @include('sections.court') --}}
-
-</x-layout>
-       <x-slot:title>
-        Home
-    </x-slot:title>
-    <h1 class="text-3xl font-bold mb-4">Welcome to PasundanPadel!</h1>
-    <p class="text-lg">Your premier destination for padel courts and services.</p>
-=======
->>>>>>> feature/loginregis
 </x-layout>
