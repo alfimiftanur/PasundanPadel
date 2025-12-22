3ceaf2f25de0e2609c14da7416893d221d23edb9
Asep Saepul Milah <146751939+asepsaepulmilah2003@users.noreply.github.com>
2025-12-22 13:36:02 +0700

    feat: menambahkan fitur CRUD jadwal lapangan

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LapanganController;
+use App\Http\Controllers\JadwalController;
 
 Route::get('/', [LapanganController::class, 'indexHome'])->name('home');
 
 Route::get('/court', [LapanganController::class, 'indexPublic'])->name('court.index');
+
 
 /* AUTH MODAL TRIGGER */
 Route::get('/login', function () {
@@ -39,4 +41,11 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::middleware(['role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
     Route::resource('lapangan', LapanganController::class);
+     // Jadwal Routes (SIMPLIFIED)
+
+
+     Route::get('/jadwal', [JadwalController::class, 'index'])->name('jadwal.index');
+    Route::get('/jadwal/edit', [JadwalController::class, 'edit'])->name('jadwal.edit');
+    Route::post('/jadwal', [JadwalController::class, 'store'])->name('jadwal.store');
+    Route::post('/jadwal/{id}/update-status', [JadwalController::class, 'updateStatus'])->name('jadwal.updateStatus');
 });
