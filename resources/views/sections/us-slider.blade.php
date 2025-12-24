738090dda473c243e872a6311989266af8cc6d29
meldawonu <trimeldawanim@gmail.com>
2025-12-24 23:48:54 +0700

    update: arrows

--- resources/views/sections/us-slider.blade.php
+++ resources/views/sections/us-slider.blade.php
@@ -6,32 +6,41 @@
 
             <div :key="index" class="transition-opacity duration-5 ease-out" x-transition.opacity>
 
-                <h4 class="italic text-teal-900 text-4xl mb-6" x-text="slides[index].tag"></h4>
+                <h4 class="font-sans italic text-teal-900 text-4xl mb-6" x-text="slides[index].tag"></h4>
 
-                <h2 class="text-3xl lg:text-4xl font-serif text-slate-900 leading-snug mb-6" x-text="slides[index].title">
-                </h2>
+                <h2 class="text-3xl lg:text-4xl font-serif text-slate-900 leading-snug mb-6" x-text="slides[index].title"></h2>
 
                 <a href="{{ route('court.index') }}"
-                    class="inline-block mt-4 text-sm uppercase tracking-wide border-b border-slate-900 pb-1">
+                    class="inline-block mt-4 text-sm font-sans uppercase tracking-wide border-b border-slate-900 pb-1">
                     Book Now
                 </a>
             </div>
 
             {{-- arrowz --}}
             <div class="flex gap-4 mt-10">
+                <!-- Prev -->
                 <button @click="prev"
-                    class="w-10 h-10 border border-slate-700 rounded-full
-                           flex items-center justify-center
-                           select-none active:scale-95">
-                    ←
+                    class="w-10 h-10 border border-slate-700 rounded-full flex items-center justify-center
+                text-slate-700 hover:bg-slate-50 select-none active:scale-95 transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                    </svg>
+
                 </button>
+
+                <!-- Next -->
                 <button @click="next"
-                    class="w-10 h-10 border border-slate-700 rounded-full
-                           flex items-center justify-center
-                           select-none active:scale-95">
-                    →
+                    class="w-10 h-10 border border-slate-700 rounded-full flex items-center justify-center
+                         text-slate-700 hover:bg-slate-50 select-none active:scale-95 transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+
                 </button>
             </div>
+
         </div>
 
         {{-- right --}}
