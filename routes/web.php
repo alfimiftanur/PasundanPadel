a724e49769a8a216368965f029e401b7233b3de8
Asep Saepul Milah <146751939+asepsaepulmilah2003@users.noreply.github.com>
2025-12-26 21:27:26 +0700

    Resolve conflict: mengambil perubahan court ke court-detail

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LapanganController;
+use App\Http\Controllers\JadwalController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [LapanganController::class, 'indexHome'])->name('home');
 
-Route::get('/court', function () {
-    return view('court.index', [
-        'title' => 'Pasundan Padel - Court List'
-    ]);
-})->name('court.index');
+Route::get('/court', [LapanganController::class, 'indexPublic'])->name('court.index');
+
+Route::get('/court/{id}', [LapanganController::class, 'showPublic'])->name('court.detail');
+
+Route::get('/schedule', [JadwalController::class, 'indexPublic'])->name('user.schedule');
+
 
 /* AUTH MODAL TRIGGER */
 Route::get('/login', function () {
@@ -24,12 +26,46 @@ Route::get('/register', function () {
         ->with('showRegister', true);
 })->name('register');
 
-Route::get('/court/{id}', function ($id) {
-    return view('court.detail');
-})->name('court.detail');
+// Booking Creation View
+Route::get('/booking/{court}', function ($court) {
+    return view('booking.create', [
+        'court' => $court
+    ]);
+})->name('booking.create');
+
+// Booking List View
+Route::get('/booking-list', function () {
+    return view('pemesanan.index', [
+        'title' => 'Kelola Booking'
+    ]);
+})->name('pemesanan.index');
+
+Route::get('/detail-booking', function () {
+    return view('pemesanan.show', [
+        'title' => 'Detail Booking',
+    ]);
+})->name('pemesanan.show');
+
+Route::get('orders-history', function (){
+    return view('booking.orders-history',[
+    ]);
+})->name('booking.oerders-history');
 
 
 /* AUTH ACTION */
-Route::post('/login', [AuthController::class, 'authenticate']);
-Route::post('/register', [AuthController::class, 'store']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+/* ADMIN ROUTES */
+Route::middleware(['role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::resource('lapangan', LapanganController::class);
+     // Jadwal Routes (SIMPLIFIED)
+
+
+     Route::get('/jadwal', [JadwalController::class, 'index'])->name('jadwal.index');
+    Route::get('/jadwal/edit', [JadwalController::class, 'edit'])->name('jadwal.edit');
+    Route::post('/jadwal', [JadwalController::class, 'store'])->name('jadwal.store');
+    Route::post('/jadwal/{id}/update-status', [JadwalController::class, 'updateStatus'])->name('jadwal.updateStatus');
+});
